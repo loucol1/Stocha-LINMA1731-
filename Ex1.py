013c6cf3dd120030d13620df65c798b3854257b8
louis <l.colin@student.uclouvain.be>
2020-04-08 16:03:08 +0200

    Fin de la loooooongue serie

--- Ex1.py
+++ Ex1.py
@@ -1,5 +1,31 @@
-
+import numpy as np
 def main():
-    print("coucouuuu sachaaa")
+    w=1
 main()
+
+
+def estimator(file_name):
+    avg_monthly_precip = np.genfromtxt(file_name, dtype='str', delimiter=", ")
+
+    nbr_tot = len(avg_monthly_precip)
+    nbr_succ = 0
+    for a in range(len(avg_monthly_precip)):
+        if avg_monthly_precip[a] == ' True':
+            nbr_succ = nbr_succ + 1
+        if avg_monthly_precip[a] == '[True':
+            nbr_succ = nbr_succ + 1
+        if avg_monthly_precip[a] == 'True]':
+            nbr_succ = nbr_succ + 1
+    print(nbr_succ)
+
+    ##MAP estimator
+    Z_theta = nbr_succ
+
+    Map_estimator = (Z_theta + 1) / (4 + nbr_tot)
+    ML_estimator = Z_theta / nbr_tot
+    print("MAP estimator = ", Map_estimator)
+    print("ML estimator = ", ML_estimator)
+
+estimator("Estimators1.txt")
+estimator("Estimators2.txt")
 
